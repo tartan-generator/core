707c42988e30f988bf61cbd5db000526dfec40b3
Juniper Jorgensen <juniper@leprechaun.dev>
2026-02-21 01:23:34 -0800

    update package version and run audit

--- default.nix
+++ default.nix
@@ -9,7 +9,7 @@
     },
 }: let
   version = (builtins.fromJSON (builtins.readFile ./package.json)).version;
-  dependencyHash = "sha256-4gYgzYJNCOPaAfvPwBpq/Y6y8SLUzo3HS44MWom8hyE=";
+  dependencyHash = "sha256-kgrTapZRcCldQVXYvQdvUl9uiapB4ImSG3igLLcsIR4=";
   buildOnVersion = nodeVersion:
     pkgs.buildNpmPackage {
       pname = "tartan-core";
